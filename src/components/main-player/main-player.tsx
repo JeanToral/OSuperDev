3d31f46f247a2c4b169af90a47f3a3fc843e0835
JeanToral <jean.toral@unoesc.edu.br>
2024-06-10 21:47:07 -0300

    end of desktop things :tada:

--- src/components/main-player/main-player.tsx
+++ src/components/main-player/main-player.tsx
@@ -14,18 +14,26 @@ function MainPlayer() {
         <div className={ 'mainPlayer'}>
             <div className={ 'mainPlayerIMG' }>
                 <img src={ player.image } alt={ player.name } />
-                <div className={ 'mainPlayerDetails' }>
-                <h2>{ player.name }</h2>
-                <p> { player.team } </p>
-                </div>
-                <div className= { 'mainPlayerActions' }>
-                <FavoriteRoundedIcon className={ 'heart' } />
-                <StarRoundedIcon className={ 'star' } />
-                <ShareRoundedIcon className={ 'share' } />
+                <div className={ 'mainPlayerActions mobile-hidden' }>
+                    <FavoriteRoundedIcon className={ 'heart' } />
+                    <StarRoundedIcon className={ 'star' } />
+                    <ShareRoundedIcon className={ 'share' } />
                 </div>
             </div>
-            <div className={ 'completeStats' }>
-                <p> Complete Stats for Fallen </p>
+                <div className={ 'mainPlayerDetails' }>
+                    <h2>{ player.name }</h2>
+                    <p className={ 'playerTeam' }> { player.team } </p>
+                    <div className={ 'completeStats mobile-hidden' }>
+                        <p> Complete Stats for { player.name } </p>
+                    </div>
+                </div>
+                <div className= { 'mainPlayerActions desktop-hidden' }>
+                    <FavoriteRoundedIcon className={ 'heart' } />
+                    <StarRoundedIcon className={ 'star' } />
+                    <ShareRoundedIcon className={ 'share' } />
+                </div>
+            <div className={ 'completeStats desktop-hidden' }>
+                <p> Complete Stats for { player.name } </p>
             </div>
         </div>
     )
